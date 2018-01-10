6d4956ac3d2f7341f37260010300054d3cdd14ef
Costin Raiciu <costin.raiciu@cs.pub.ro>
2018-01-10 17:38:19 +0200

    P4Bugs

--- src/test/scala/parser/p4/test/P4Bugs.scala
+++ src/test/scala/parser/p4/test/P4Bugs.scala
@@ -4,13 +4,14 @@ import java.io.{BufferedOutputStream, FileOutputStream, PrintStream}
 
 import org.change.parser.p4.ControlFlowInterpreter
 import org.change.parser.p4.factories.{InstanceBasedInitFactory, SymbolicRegistersInitFactory}
+import org.change.parser.p4.tables.SymbolicSwitchInstance
 import org.change.utils.prettifier.JsonUtil
 import org.change.v2.analysis.executor.CodeAwareInstructionExecutor
 import org.change.v2.analysis.executor.solvers.Z3BVSolver
 import org.change.v2.analysis.expression.concrete.ConstantValue
 import org.change.v2.analysis.memory.{State, Tag}
 import org.change.v2.analysis.processingmodels.instructions._
-import org.change.v2.p4.model.SwitchInstance
+import org.change.v2.p4.model.{Switch, SwitchInstance}
 import org.scalatest.FunSuite
 import org.change.v2.analysis.memory.TagExp.IntImprovements
 
@@ -155,6 +156,28 @@ class P4Bugs extends FunSuite {
     val relevant = failed
     printResults(dir, port, ok, relevant, "bad")
   }
+
+  test("INTEGRATION - copy-to-cpu star entries") {
+    val dir = "inputs/copy-to-cpu/"
+    val p4 = s"$dir/copy_to_cpu-ppc.p4"
+    val dataplane = s"$dir/commands_star.txt"
+
+    val switchInstance = SymbolicSwitchInstance.fromFileWithSyms("router", Map[Int, String](1 -> "veth0", 2 -> "veth1", 3 -> "cpu"),
+      Map[Int, Int](250 -> 3), Switch.fromFile(p4), dataplane)
+    val res = new ControlFlowInterpreter(switchInstance, switchInstance.switch)
+
+    val port = 1
+    val ib = InstructionBlock(
+      Forward(s"router.input.$port")
+    )
+    val codeAwareInstructionExecutor = CodeAwareInstructionExecutor(res.instructions(), res.links(), solver = new Z3BVSolver)
+    val (initial, _) = codeAwareInstructionExecutor.
+      execute(InstructionBlock(res.allParserStatesInstruction()), State.clean, verbose = true)
+    val (ok: List[State], failed: List[State]) = executeAndPrintStats(ib, initial, codeAwareInstructionExecutor)
+    printResults(dir, port, ok, failed, "good")
+  }
+
+
 
   test("INTEGRATION - ndp_router reg access test") {
     val dir = "inputs/ndp-router-reg-access/"
