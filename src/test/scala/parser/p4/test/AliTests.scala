87c14e6f451d4b35cd033986df8f8db0e39ab317
shell <shellqiqi@outlook.com>
2021-01-12 19:47:17 +0800

    Add simple router

--- src/test/scala/parser/p4/test/AliTests.scala
+++ src/test/scala/parser/p4/test/AliTests.scala
@@ -13,6 +13,29 @@ import org.change.v2.p4.model.Switch
 import org.scalatest.FunSuite
 
 class AliTests extends FunSuite {
+  test("Simple Router") {
+    val dir = "inputs/simple-router-testing/"
+    val p4 = s"$dir/simple_router.p4"
+    val dataplane = s"$dir/commands.txt"
+    val res = ControlFlowInterpreter(
+      p4,
+      dataplane,
+      Map[Int, String](1 -> "veth0", 2 -> "veth1"),
+      "router",
+      optAdditionalInitCode = Some((x, y) => {
+        new SymbolicRegistersInitFactory(x).initCode()
+      }))
+    val port = 1
+    val ib = InstructionBlock(
+      Forward(s"router.input.$port")
+    )
+    val codeAwareInstructionExecutor = CodeAwareInstructionExecutor(res.instructions(), res.links(), solver = new Z3BVSolver)
+    val (initial, _) = codeAwareInstructionExecutor.
+      runToCompletion(InstructionBlock(res.allParserStatesInstruction()), State.clean, verbose = true)
+    val (ok: List[State], failed: List[State]) = executeAndPrintStats(ib, initial, codeAwareInstructionExecutor)
+    // printResults(dir, port, ok, failed, "ndp")
+  }
+
   test("NDP") {
     val dir = "ali_inputs/ndp/"
     val p4 = s"$dir/ndp_router-ppc.p4"
